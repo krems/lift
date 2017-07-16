12ee808a3625c1b881f1572af5b2c12474d80563
krems <kremsnx@gmail.com>
2017-07-16 15:51:15 +0300

    Tests for command line part supplied

---
+++ src/test/java/ru/ovchinnikov/CommandListenerTest.java
@@ -0,0 +1,16 @@
+package ru.ovchinnikov;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CommandListenerTest {
+    private CommandListener commandListener = new CommandListener(0, 3, 2, 0);
+
+    @Test
+    public void calculateOneFloorPassTimeMillis_shouldCorrectlyCalculateTime() {
+        long timeMillis = commandListener.calculateOneFloorPassTimeMillis(3, 2);
+        assertEquals(1500, timeMillis);
+    }
+}
--- src/test/java/ru/ovchinnikov/MainTest.java
+++ src/test/java/ru/ovchinnikov/MainTest.java
@@ -1,21 +1,224 @@
 package ru.ovchinnikov;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
+import org.junit.Before;
+import org.junit.Test;
 
-public class MainTest extends TestCase {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-    public MainTest(String testName) {
-        super(testName);
+public class MainTest {
+    private boolean success = false;
+    private Main main = new Main() {
+        @Override
+        protected CommandListener buildCommandListener(final int floorsNum, final double floorHeight,
+                                                       final double speed, final int doorsOpenedTime) {
+            return new CommandListener(floorsNum, floorHeight, speed, doorsOpenedTime) {
+                @Override
+                public void start() {
+                    success = true;
+                }
+            };
+        }
+    };
+
+    @Before
+    public void setUp() {
+        success = false;
     }
 
-    public static Test suite() {
-        return new TestSuite(MainTest.class);
+    @Test
+    public void testParseArgs_shouldSuccess_whenAllValidShortArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertTrue(success);
     }
 
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    public void testParseArgs_shouldSuccess_whenAllValidLongArgsProvided() {
+        String[] args = {
+                "--floors", "6",
+                "--floor-height", "2.7",
+                "--velocity", "3.1",
+                "--opened-time", "7"
+        };
+        main.parseArgs(args);
+        assertTrue(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoArgsProvided() {
+        String[] args = {};
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoFloorArgsProvided() {
+        String[] args = {
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoHeightArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoVelocityArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "2.7",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoOpenedDoorsTimeoutArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "2.7",
+                "-v", "3.1"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenFloorArgIsNotInteger() {
+        String[] args = {
+                "-f", "6.3",
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenDoorsTimeoutArgIsNotInteger() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7.22"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenFloorHeightArgIsNotFloat() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "ABCD",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenVelocityArgIsNotFloat() {
+        String[] args = {
+                "-f", "6.3",
+                "-fh", "2.7",
+                "-v", "EFGH",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenFloorArgIsNotInValidRange_LessThanMin() {
+        String[] args = {
+                "-f", String.valueOf(Main.MIN_NUMBER_OF_FLOORS - 1),
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenFloorArgIsNotInValidRange_MoreThanMax() {
+        String[] args = {
+                "-f", String.valueOf(Main.MAX_NUMBER_OF_FLOORS + 1),
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoArgForFloorArgsProvided() {
+        String[] args = {
+                "-f",
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoArgForHeightArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-fh",
+                "-v", "3.1",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoArgForVelocityArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "2.7",
+                "-v",
+                "-ot", "7"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
+    }
+
+    @Test
+    public void testParseArgs_shouldFail_whenNoArgForDoorsTimeoutArgsProvided() {
+        String[] args = {
+                "-f", "6",
+                "-fh", "2.7",
+                "-v", "3.1",
+                "-ot"
+        };
+        main.parseArgs(args);
+        assertFalse(success);
     }
 }
